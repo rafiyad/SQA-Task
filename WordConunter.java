8c23e210234599ff1d5daa0979c6ea422416dc30
Rafiyad Darazat <141349190+rafiyad@users.noreply.github.com>
2024-06-10 12:04:36 +0600

    Task submission for SQA

---
+++ WordConunter.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class WordConunter {
+    public static void main(String[] args) throws IOException {
+        String str = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            str = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(str);
+
+        str=str.trim().replaceAll("\\p{Punct}","").toLowerCase();
+        System.out.println(str);
+        String words []= str.split(" ");
+        TreeMap<String, Integer> wCount = new TreeMap<>();
+        for (String word : words) {
+            wCount.put(word, wCount.getOrDefault(word, 0)+1);
+        }
+
+        for (Map.Entry<String, Integer> entry : wCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
+
